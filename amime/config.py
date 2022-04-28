c854bce5022d532754b13fd9ee7711268894f7d2
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-04-28 11:50:59 +0700

    Add files via upload

---
+++ amime/config.py
@@ -0,0 +1,19 @@
+from typing import Dict, List
+
+
+GROUPS: Dict = {
+    "en": -1001519273319,
+    "pt": -1001519273319,
+}
+CHANNELS: Dict = {
+    "en": -1001706431524,
+    "pt": -1001706431524,
+}
+CHATS: Dict = {
+    "backup": 10,
+    "requests": -1001519273319,
+    "videos": -1001559315851,
+    "staff": -1001519273319,
+}
+SUDO_USERS: List[int] = [1765145725]  # @AndrielFR, @Hitalo and @VegaZS
+PREFIXES: List[str] = ["!", "/"] # Like !start or /start
